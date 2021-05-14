1a7f71e75fce138a9118810b2c6414c27c683f05
Joko Prasetyo <robertkwan.rk@gmail.com>
2021-05-14 23:56:22 +0700

    Changed chatId to userId

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const TelegramBot = require("node-telegram-bot-api");
 const TinyURL = require("tinyurl");
 module.exports = {
   init(providerOptions) {
-    const { chatId, token } = providerOptions;
+    const { userId, token } = providerOptions;
     const bot = new TelegramBot(token, { polling: true });
     return {
       async upload(file) {
-        const result = await bot.sendDocument(
-          chatId,
-          file.buffer, {
-            caption: file.name,
-            disable_notification: true
-          }
-        );
+        const result = await bot.sendDocument(userId, file.buffer, {
+          caption: file.name,
+          disable_notification: true,
+        });
         const fileUrl = await bot.getFileLink(result.document.file_id);
         const shortenUrl = await TinyURL.shorten(fileUrl);
         file.url = shortenUrl;
@@ -23,7 +20,7 @@ module.exports = {
       },
       async delete(file) {
         try {
-          await bot.deleteMessage(chatId, file.alternativeText.toString());
+          await bot.deleteMessage(userId, file.alternativeText.toString());
         } catch (e) {
           console.log(e);
         }
