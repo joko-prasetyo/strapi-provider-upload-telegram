60c449fbd21d027e60aac0d797b31725826515ae
Joko Prasetyo <robertkwan.rk@gmail.com>
2021-05-15 05:57:05 +0700

    Add disable notification option & Change readme

--- README.md
+++ README.md
@@ -3,17 +3,19 @@
 A simplistic telegram upload provider for strapi.
 
 **Usage**
-
 ```
 ./config/plugins.js
+```
+
+```
 module.exports = ({ env }) => ({
   upload: {
     provider: "telegram",
     providerOptions: {
       token: <YOUR TELEGRAM BOT TOKEN>,
-      userId: <YOUR USER ID>,
+      userId: <YOUR TELEGRAM USER ID>,
     },
   },
 });
 ```
-You can use [@BotFather](https://t.me/BotFather) for generating telegram token and user id by using this [bot](https://t.me/userinfobot).
+You can use [@BotFather](https://t.me/BotFather) for generating telegram token and userId by using this [bot](https://t.me/userinfobot).
--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const TelegramBot = require("node-telegram-bot-api");
 const TinyURL = require("tinyurl");
 module.exports = {
   init(providerOptions) {
-    const { userId, token } = providerOptions;
+    const { userId, token, disableNotification } = providerOptions;
     const bot = new TelegramBot(token, { polling: true });
     return {
       async upload(file) {
         const result = await bot.sendDocument(userId, file.buffer, {
           caption: file.name,
-          disable_notification: true,
+          disable_notification: disableNotification,
         });
         const fileUrl = await bot.getFileLink(result.document.file_id);
         const shortenUrl = await TinyURL.shorten(fileUrl);
